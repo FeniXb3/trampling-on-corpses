2d2de85a1f79305d87def16f2a4b2f649a18011b
FeniXb3 <fenix.b3@gmail.com>
2018-12-02 17:11:37 +0100

    Add basic logic of deadly spikes

---
+++ Assets/Scripts/Dead.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Dead : MonoBehaviour
+{
+    private void Start()
+    {
+        DisableInput();
+        FadeOutCorpse();
+    }
+
+    private void DisableInput()
+    {
+        gameObject.GetComponent<Player>().enabled = false;
+    }
+
+    private void FadeOutCorpse()
+    {
+        gameObject.GetComponent<GrayOut>().enabled = true;
+    }
+}
