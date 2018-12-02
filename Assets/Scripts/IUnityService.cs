96ac4f544b5aaeac4de9d8df8b3a091d035816d8
Konrad Gadzina <fenix.b3@gmail.com>
2018-12-02 11:23:05 +0100

    Update Assets/Scripts/IUnityService.cs
    Co-Authored-By: tomaszFijalkowski <36127984+tomaszFijalkowski@users.noreply.github.com>

--- Assets/Scripts/IUnityService.cs
+++ Assets/Scripts/IUnityService.cs
@@ -6,7 +6,7 @@ public interface IInputService
     bool GetButton(string buttonName);
 }
 
-class UnityService : IUnityService
+class UnityInputService : IInputService
 {
     public float GetDeltaTime()
     {
