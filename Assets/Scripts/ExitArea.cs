6aced33afc0674a7d22db4ad02b974e5893fe412
Tomasz Fijalkowski <tomelecf@gmail.com>
2018-12-02 18:22:17 +0100

    Added intro scene

---
+++ Assets/Scripts/ExitArea.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class ExitArea : MonoBehaviour
+{
+    [SerializeField] private SceneController sceneController;
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            sceneController.NextLevel();
+        }
+    }
+}
