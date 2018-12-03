5ee38b9ee88a18e512397d185a9d266efea1e678
FeniXb3 <fenix.b3@gmail.com>
2018-12-03 16:59:55 +0100

    Add saving player color to prefs and reading it when player is created

---
+++ Assets/Scripts/PlayerPrefsExtensions.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public static class PlayerPrefsExtensions
+{
+    public static void SetColor(string name, Color value)
+    {
+        PlayerPrefs.SetFloat(string.Format("{0}_r", name), value.r);
+        PlayerPrefs.SetFloat(string.Format("{0}_g", name), value.g);
+        PlayerPrefs.SetFloat(string.Format("{0}_b", name), value.b);
+    }
+    
+    public static Color GetColor(string name)
+    {
+        var r = PlayerPrefs.GetFloat(string.Format("{0}_r", name));
+        var g = PlayerPrefs.GetFloat(string.Format("{0}_g", name));
+        var b = PlayerPrefs.GetFloat(string.Format("{0}_b", name));
+        
+        return new Color(r, g, b);
+    }
+
+    public static bool HasColorKey(string key)
+    {
+        var rKey = PlayerPrefs.HasKey(string.Format("{0}_r", key));
+        var gKey = PlayerPrefs.HasKey(string.Format("{0}_g", key));
+        var bKey = PlayerPrefs.HasKey(string.Format("{0}_b", key));
+
+        return rKey && gKey && bKey;
+    }
+}
