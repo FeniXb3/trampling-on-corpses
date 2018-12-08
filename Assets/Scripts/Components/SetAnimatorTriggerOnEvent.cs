101f491ee37812a2ed4be830c44ea007f1e33ba2
FeniXb3 <fenix.b3@gmail.com>
2018-12-08 23:12:12 +0100

    Make component setting animator trigger more general

---
+++ Assets/Scripts/Components/SetAnimatorTriggerOnEvent.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class SetAnimatorTriggerOnEvent : MonoBehaviour
+{
+	[SerializeField] private Animator animator;
+	[SerializeField] private EventType eventType;
+	[SerializeField] private string triggerName;
+	
+	private void OnEnable()
+	{
+		EventManager.Instance.AddListener(eventType, OnEvent);
+	}
+
+	private void OnDisable()
+	{
+		EventManager.Instance.RemoveListener(eventType, OnEvent);
+	}
+
+	private void OnEvent(object arg)
+	{
+		animator.SetTrigger(triggerName);
+	}
+}
