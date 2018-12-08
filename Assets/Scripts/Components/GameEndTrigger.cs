d26a679988cf0e8adc078eb9a2facc642c45e690
FeniXb3 <fenix.b3@gmail.com>
2018-12-09 00:05:06 +0100

    Change setting end text to be handled as end game event listener

--- Assets/Scripts/Components/GameEndTrigger.cs
+++ Assets/Scripts/Components/GameEndTrigger.cs
@@ -1,24 +1,12 @@
-﻿using System.Collections;
-using UnityEngine;
-using UnityEngine.UI;
+﻿using UnityEngine;
 
 public class GameEndTrigger : MonoBehaviour
 {
-    [SerializeField] private Text sacrifices;
-
     private void OnTriggerEnter2D(Collider2D col)
     {
         if (col.CompareTag("Player"))
         {
             EventManager.Instance.TriggerEvent(EventType.EndGame, Dead.TotalDeaths);
-            
-            StartCoroutine(ShowEndScreen());
         }
-    }
-
-    IEnumerator ShowEndScreen()
-    {
-        yield return new WaitForSeconds(1.5f);
-        sacrifices.text = $"you sacrificed {Dead.TotalDeaths} lives";
     }
 }
---
+++ Assets/Scripts/Components/SetTextDelayedOnEvent.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetTextDelayedOnEvent : MonoBehaviour
+{
+	[SerializeField] private Text textComponent;
+	[SerializeField] private EventType eventType;
+	[Tooltip("This string will be replaced with parameter sent when event was triggered")]
+	[SerializeField] private string placeholder = "{arg}";
+	[SerializeField] private string textTemplate;
+	[SerializeField] private float delay = 1.5f;
+
+	private void OnEnable()
+	{
+		EventManager.Instance.AddListener(eventType, OnEvent);
+	}
+
+	private void OnDisable()
+	{
+		EventManager.Instance.RemoveListener(eventType, OnEvent);
+	}
+
+	private void OnEvent(object arg)
+	{
+		var value = arg.ToString();
+		StartCoroutine(SetTextDelayed(value));
+	}
+	
+	IEnumerator SetTextDelayed(string value)
+	{
+		yield return new WaitForSeconds(delay);
+		textComponent.text = textTemplate.Replace(placeholder, value);
+	}
+}
