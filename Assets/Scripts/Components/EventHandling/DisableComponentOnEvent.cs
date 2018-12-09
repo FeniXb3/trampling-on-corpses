ab51ca498ec08b61df1c69d7a5d2d0fb892f09e7
FeniXb3 <fenix.b3@gmail.com>
2018-12-09 01:05:58 +0100

    Fix bug of hiding bodies of dead player characters on game end

---
+++ Assets/Scripts/Components/EventHandling/DisableComponentOnEvent.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class DisableComponentOnEvent : MonoBehaviour
+{
+	[SerializeField] private MonoBehaviour component;
+	[SerializeField] private EventType eventType;
+	[SerializeField] private bool runOnce = true;
+	
+	private void OnEnable()
+	{
+		EventManager.Instance.AddListener(eventType, OnEvent);
+	}
+
+	private void OnDisable()
+	{
+		EventManager.Instance.RemoveListener(eventType, OnEvent);
+	}
+
+	private void OnEvent(object arg)
+	{
+		component.enabled = false;
+		
+		if (runOnce)
+		{
+			enabled = false;
+		}
+	}
+
+}
